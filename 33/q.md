b64ce15b9e74ee7ae76b10c8502e1299fba7b550
pritam <itspritamprasad@gmail.com>
2025-12-04 13:26:29 +0530

    Q2: Print only even number from 1 to 20 use a loop and condition to print only even ones.

--- 33/q.md
+++ 33/q.md
@@ -1,1 +1,2 @@
 1. Print numbers from 1 to 10 Loop from 1 to 10 and print each number.
+2. Print only even number from 1 to 20 use a loop and condition to print only even ones.
--- 33/script.js
+++ 33/script.js
@@ -4,3 +4,18 @@
     console.log(i);
   }
 }
+
+/*Print only even number from 1 to 20 use a loop and condition to print only even ones.
+ */
+//method 1
+{
+  for (let i = 1; i < 21; i++) {
+    if (i % 2 === 0) console.log(i);
+  }
+}
+//method 2
+{
+  for (let i = 2; i < 21; i += 2) {
+    console.log(i);
+  }
+}
