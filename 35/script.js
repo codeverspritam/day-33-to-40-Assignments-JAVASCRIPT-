b80c41011605d32747d5da01a4ae5a2df7c10e9e
pritam <itspritamprasad@gmail.com>
2025-12-06 15:28:49 +0530

    Q14: Sum of all odd numbers from 1 to 30 Add only odd numbers. Print final sum.

--- 35/script.js
+++ 35/script.js
@@ -70,3 +70,13 @@ Use modulo % and loop. */
     }
   }
 }
+
+/*Sum of all odd numbers from 1 to 30
+Add only odd numbers. Print final sum. */
+{
+  for (let i = 1; i <= 30; i++) {
+    if (i % 2 === 1) {
+      console.log(i);
+    }
+  }
+}
