2f35459b546f3abe05f637c07ecc308709fb6cb4
pritam <itspritamprasad@gmail.com>
2025-12-20 18:01:44 +0530

    Q1: Write a higher-order function runTwice(fn) that takes another function and executes it two times

---
+++ 41/script.js
@@ -0,0 +1,10 @@
+/*Q1: Write a higher-order function runTwice(fn) that takes another function and executes it two times.*/
+{
+  function runTwice(fn) {
+    fn();
+    fn();
+  }
+  runTwice(function () {
+    console.log("Hello");
+  });
+}
