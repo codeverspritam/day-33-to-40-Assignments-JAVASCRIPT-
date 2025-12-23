50047f3886f070eb022bd335a49a6491fc74b694
pritam <itspritamprasad@gmail.com>
2025-12-23 16:25:32 +0530

    addEventListener

--- 44/script.js
+++ 44/script.js
@@ -1,2 +1,21 @@
 let btn = document.querySelector("button");
 let main = document.querySelector("main");
+
+btn.addEventListener("click", function () {
+  let div = document.createElement("div");
+  let x = Math.random() * 100;
+  let y = Math.random() * 100;
+  let r = Math.random() * 360;
+  let c1 = Math.floor(Math.random() * 100);
+  let c2 = Math.floor(Math.random() * 100);
+  let c3 = Math.floor(Math.random() * 100);
+  div.style.height = "50px";
+  div.style.width = "50px";
+  div.style.position = "absolute";
+  div.style.left = x + "%";
+  div.style.top = y + "%";
+  div.style.rotate = r + "deg   ";
+  div.style.backgroundColor = `rgb(${c1},${c2},${c3})`;
+  main.appendChild(div);
+  console.log(div);
+});
