c506f94aef69e0436fa1434c3320cd43d4cb0bcf
pritam <itspritamprasad@gmail.com>
2025-12-24 17:52:26 +0530

    addEventListener

--- 45/script.js
+++ 45/script.js
@@ -2,3 +2,10 @@ let grow = 0;
 let btn = document.querySelector("button");
 let h2 = document.querySelector("h2");
 let inner = document.querySelector(".inner");
+
+btn.addEventListener("click", function () {
+  btn.style.pointerEvents = "none";
+  let num = 60 + Math.floor(Math.random() * 50);
+
+
+});
